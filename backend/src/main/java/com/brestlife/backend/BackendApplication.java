1a90c4c1deff03f4b67c981bed3f4e42e4efa207
Mathurin <115731176+mathurinhauville@users.noreply.github.com>
2025-01-16 13:48:54 +0100

    gestion env application.properties

--- backend/src/main/java/com/brestlife/backend/BackendApplication.java
+++ backend/src/main/java/com/brestlife/backend/BackendApplication.java
@@ -1,13 +1,21 @@
 package com.brestlife.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                .directory("../")
+                .load();
+        System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
+        System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
+        System.setProperty("POSTGRES_DB", dotenv.get("POSTGRES_DB"));
+
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
 }
