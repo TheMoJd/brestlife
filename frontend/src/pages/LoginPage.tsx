b777f697eb230d5e57c495660d24633b5816a48e
TheMoJd <moetez.jaoued@etudiant.univ-brest.fr>
2025-03-06 22:42:40 +0100

    refactor(LoginPage): adjust flex properties for improved layout

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -30,7 +30,7 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen flex justify-center items-center bg-gray-50  px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8 bg-white p-6 rounded-lg shadow-md">
         <div>
           <h2 className="text-center text-3xl font-extrabold text-gray-900">
