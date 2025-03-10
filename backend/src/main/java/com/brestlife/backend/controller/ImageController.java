5eb8897b251ffef0263c42b95b709cd48e0e3a5e
Mathurin <115731176+mathurinhauville@users.noreply.github.com>
2025-03-10 09:17:47 +0100

    refactor(image): remove unnecessary request mapping and update MinIO properties for security

--- backend/src/main/java/com/brestlife/backend/controller/ImageController.java
+++ backend/src/main/java/com/brestlife/backend/controller/ImageController.java
@@ -11,7 +11,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/photos")
 public class ImageController implements ImagesApi {
 
     @Autowired
