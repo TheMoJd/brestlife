38ee3d295a975b9687ed8d6f1805979ed2a0258e
mathurin <mathurinhauville@gmail.com>
2025-03-12 20:16:38 +0100

    refactor(mapstruct): remove unused import in UserMapper

--- backend/src/main/java/com/brestlife/backend/mapper/UserMapper.java
+++ backend/src/main/java/com/brestlife/backend/mapper/UserMapper.java
@@ -3,7 +3,6 @@ package com.brestlife.backend.mapper;
 import com.brestlife.backend.entity.UserEntity;
 import com.brestlife.generate.dto.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
