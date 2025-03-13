9d648d533ddeda541cb0253604aed3b992be7122
Mathurin <115731176+mathurinhauville@users.noreply.github.com>
2025-03-13 12:15:07 +0100

    feat: configure API client with dynamic base URL from environment variable

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,13 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import {AuthProvider} from './contexts/AuthProvider.tsx';
+import {client} from "./gen/openapi/client.gen.ts";
+
+client.setConfig(
+    {
+        baseUrl: import.meta.env.VITE_API_URL || 'http://localhost:8080/api'
+    }
+)
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
