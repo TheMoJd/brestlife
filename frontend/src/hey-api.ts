d93ce29a878fadaa59c989f321a6d5cf1d2c41d3
Mathurin <115731176+mathurinhauville@users.noreply.github.com>
2025-03-13 13:52:25 +0100

    fix: update API base URL in client configuration

--- frontend/src/hey-api.ts
+++ frontend/src/hey-api.ts
@@ -1,13 +1,13 @@
-import { CreateClientConfig } from "@hey-api/client-next";
+import {CreateClientConfig} from "@hey-api/client-next";
 
 export const createClientConfig: CreateClientConfig = (config) => {
     const token = localStorage.getItem("token");
 
     return {
         ...config,
-        baseUrl: import.meta.env.VITE_API_URL || 'http://localhost:8080/hey-api',
+        baseUrl: import.meta.env.VITE_API_URL || 'http://localhost:8080/api',
         headers: {
-            ...(token ? { Authorization: `Bearer ${token}` } : {})
+            ...(token ? {Authorization: `Bearer ${token}`} : {})
         }
     };
 };
